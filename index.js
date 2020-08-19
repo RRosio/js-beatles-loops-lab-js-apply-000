4f4ee9bf612b77036f52d2c0dec1748849c4769b
Rosio G Reyes <rosio_reyes@yahoo.com>
2020-08-19 02:33:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // add solution here
+var i;
 function theBeatlesPlay(arrMus, arrIns){
   var myArr = new Array();
-  for(int i =0; i < arrMus; i++){
+  for(i =0; i < arrMus; i++){
     var myStr = arrMus[i] + " plays " + arrIns[i];
     myArr.push(myStr);
   }
