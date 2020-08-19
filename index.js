9410f66e066f82e909d387d75d2aff62899cd071
Rosio G Reyes <rosio_reyes@yahoo.com>
2020-08-19 02:31:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 function theBeatlesPlay(arrMus, arrIns){
   var myArr = new Array();
   for(int i =0; i < arrMus; i++){
-    
+    var myStr = arrMus[i] + " plays " + arrIns[i];
+    myArr.push(myStr);
   }
+  return myArr
 }
